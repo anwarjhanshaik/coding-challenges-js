af66c971efb2d1de37c3c6607869e1556c7429e6
Shaik Sajida <anwarjhanshaik@gmail.com>
2026-07-23 11:00:49 +0530

    Implement Chunky Monkey Algorithm

---
+++ js-practice/chunky-monkey-algorithm.js
@@ -0,0 +1,14 @@
+//fCC Implement the Chunky Monkey Algorithm
+function chunkArrayInGroups(arr, num) {
+  const chunkArray = [];
+  for (let i = 0; i < arr.length; i+=num) {
+    chunkArray.push(arr.slice(i, i+num));
+  }
+  return chunkArray;
+}
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2));
